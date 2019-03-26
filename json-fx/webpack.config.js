a08a75873c6372ab101a57674a2d96c48206ba17
Robb Eads <robb@eadsfamily.us>
2019-03-26 16:44:21 -0400

    build script tweaks

--- json-fx/webpack.config.js
+++ json-fx/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: {
-    app: ["./dist/js/index.js"]
+    app: ["./dist/full/index.js"]
   },
   output: {
     path: path.resolve(__dirname, "./dist/webpack"),
