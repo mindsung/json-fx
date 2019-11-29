87516fde7309d46861f2d20afb37ce6b2da617b6
Robb Eads <robb@eadsfamily.us>
2019-11-29 17:45:10 -0500

    fix create unit tests from examples

--- json-fx/src/tests/scripts/dynamic-field.spec.ts
+++ json-fx/src/tests/scripts/dynamic-field.spec.ts
@@ -1,43 +1,6 @@
 import { describe, it } from "mocha";
 import { assert } from "chai";
 import { ScriptTester } from "../script-tester";
-import { isObject } from "../../common";
-
-function separateExpects(templ: any, path: string = ""): { template: any, expects: any } {
-  return Object.keys(templ).reduce((acc, key) => {
-    const value = templ[key];
-
-    if (key.endsWith("-expects")) {
-      const trimKey = key.substring(0, key.indexOf("-expects"));
-      const subpath = path ? path + "." + trimKey : trimKey;
-
-      acc.expects[subpath] = value;
-    } else {
-      if (isObject(value)) {
-        const subpath = path != "" ? path + "." + key : key;
-        const sep = separateExpects(value, subpath);
-        acc.template[key] = sep.template;
-        Object.assign(acc.expects, sep.expects);
-      } else {
-        acc.template[key] = value;
-      }
-    }
-    return acc;
-  }, { template: {}, expects: {} });
-}
-
-function assertExpects(output: any, expects: any): void {
-  for (const eKey of Object.keys(expects)) {
-    const path = eKey.split(".");
-    let actual = output;
-
-    for (const pathItem of path) {
-      actual = actual[pathItem];
-    }
-
-    assert.deepEqual(actual, expects[eKey]);
-  }
-}
 
 describe("Scripts [dynamic fields]", function (): void {
 
@@ -81,11 +44,5 @@ describe("Scripts [dynamic fields]", function (): void {
   it("Evaluates literal key", function (): void {
     const result = tester.run("'a b c'");
     // assert.deepEqual(result, {"a-plus-b": 11});
-  });
-
-  it("Separates expects", function (): void {
-    const templ = { "foo": { "a": 10, "a-expects": 10 }, "foo-expects": { "a": 10 } };
-    const result = separateExpects(templ);
-    assertExpects(tester.run(result.template), result.expects);
   });
 });
---
+++ json-fx/src/tests/scripts/test-examples.spec.ts
@@ -0,0 +1,53 @@
+import * as fs from "fs";
+import { JsonFx } from "../..";
+import { describe, it } from "mocha";
+import { assert } from "chai";
+import { isObject } from "../../common";
+
+function separateExpects(template: any, path: string = ""): { template: any, expects: any } {
+  return Object.keys(template).reduce((acc, key) => {
+    const value = template[key];
+
+    if (key.endsWith("-expect")) {
+      const trimKey = key.substring(0, key.indexOf("-expect"));
+      const subpath = path ? path + "." + trimKey : trimKey;
+
+      acc.expects[subpath] = value;
+    } else {
+      if (isObject(value)) {
+        const subpath = path != "" ? path + "." + key : key;
+        const sep = separateExpects(value, subpath);
+        acc.template[key] = sep.template;
+        Object.assign(acc.expects, sep.expects);
+      } else {
+        acc.template[key] = value;
+      }
+    }
+    return acc;
+  }, { template: {}, expects: {} });
+}
+
+function testExpects(output: any, expects: any): void {
+  for (const eKey of Object.keys(expects)) {
+    const path = eKey.split(".");
+    let actual = output;
+
+    for (const pathItem of path) {
+      actual = actual[pathItem];
+    }
+
+    it(eKey, () => {
+      assert.deepEqual(actual, expects[eKey]);
+    });
+  }
+}
+
+// TODO: Recurse into directories
+
+fs.readdirSync("../examples").filter(f => f.endsWith(".fx.json")).forEach(f => {
+  describe("Test example file: " + f, () => {
+    const te = separateExpects(JSON.parse(fs.readFileSync("../examples/" + f).toString()));
+    const output = new JsonFx().compile(te.template).evaluate();
+    testExpects(output, te.expects);
+  });
+});
