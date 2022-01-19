d173fe543f9b21d4422d311cf1ecd847294bcc70
Robb Eads <robb.eads@viavisolutions.com>
2022-01-19 17:33:00 -0500

    tests

--- json-fx/src/tests/scripts/misc-debug.spec.ts
+++ json-fx/src/tests/scripts/misc-debug.spec.ts
@@ -8,37 +8,33 @@ describe("Scripts [misc/debug]", function (): void {
 
   tester.input({ name: "$", value: {
     a: [
-      { b: [1, 2, 3, 4] },
-      { b: [5, 6, 7] }
+      { b: [{ c: [ 1, 2, 3, 4] }, { c: [1, 2, 3] }] },
+      { b: [{ c: [ 1, 2, 3] }, { c: [1, 2, 3, 4, 5, 6] }, { c: [1, null, 3, 4] }] }
     ]
   }});
 
-  it("Doesn't f*#! this up", function (): void {
+  it("It seems to f*#! this up", function (): void {
     const result = tester.run({
-      "@addValues($items, @valueExpr)": {
+      "@addValues($items, $accumValue, @valueExpr)": {
         "@reducer($total, $item)": {
-          "$itemTotal": "ifElse($item != null, @valueExpr($item), 0)",
+          "$itemTotal": "ifElse($item != null, @valueExpr($item, $total), 0)",
           "()": "ifElse($itemTotal != null, log('total:', $total) + log('add:', $itemTotal), log('no-add-total', $total))"
         },
-        "()": "$items?:reduce(@reducer, 0)"
+        "()": "log('reduce result:', log('accum:', $accumValue) + $items?:reduce(@reducer, $accumValue))"
       },
-      "@countLevel2($a)": "@addValues($a.b, $n => 1)",
-      "()": "@addValues($.a, @countLevel2)"
-    });
-    assert.deepEqual(result, 7);
-  });
-
-  it("Doesn't f*#! this up either", function (): void {
-    const result = tester.run({
-      "@echo($val)": {
-        "@anotherEcho($val2)": {
-          "$ret": "$val2",
-          "()": "$ret"
-        },
-        "()": "@anotherEcho($val)"
-      },
-      "()": "@echo(2) + @echo(3)"
+      "@countLevel2($a, $prevValue)": "@addValues($a.b, $prevValue, $n => 1)",
+      "()": "@addValues($.a, 0, @countLevel2)"
     });
     assert.deepEqual(result, 5);
+  });
+
+  it("This works", function (): void {
+    const totals = tester.run({
+      "@addTotals($t1, $t2)": "{ count: ($t1?.count || 0) + ($t2?.count || 0), sum: ($t1?.sum || 0) + ($t2?.sum || 0) }",
+      "@totalCs($cs)": "$cs:reduce(($total, $c) => @addTotals($total, ifElse($c != null, { count: 1, sum: $c })))",
+      "@totalBs($bs)": "$bs:reduce(($total, $b) => @addTotals($total, ifElse($b != null, @totalCs($b.c))))",
+      "()": "$.a?:reduce(($total, $a) => @addTotals($total, ifElse($a != null, @totalBs($a.b))))"
+    });
+    assert.deepEqual(totals, { count: 19, sum: 51 });
   });
 });
